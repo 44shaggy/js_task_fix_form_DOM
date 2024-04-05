f289de1b2732e6acc23493af283fca9296598afa
Emil Sukiennik <ewilemil@gmail.com>
2024-04-05 16:53:41 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const inputs = document.querySelectorAll("input");
+[
+    ...inputs
+].forEach((input)=>{
+    const label = document.createElement("label");
+    label.classList.add("field-label");
+    label.for = input.id;
+    const camelCaseToSpaces = (camel)=>camel.split(/(?=[A-Z])/).join(" ");
+    label.textContent = camelCaseToSpaces(input.name);
+    input.placeholder = camelCaseToSpaces(input.name[0].toUpperCase() + input.name.slice(1));
+    input.parentElement.append(label);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
